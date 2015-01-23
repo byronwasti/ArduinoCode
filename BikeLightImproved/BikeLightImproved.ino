8d2e0a6f1e92c78333cdddc56306df3ec2b2796b
Byron Wasti <byron.wasti@gmail.com>
2015-01-22 21:05:43 -0500

    Added OutIn functionality

--- BikeLightImproved/BikeLightImproved.ino
+++ BikeLightImproved/BikeLightImproved.ino
@@ -10,12 +10,10 @@ void setup(){
     pinMode(5, OUTPUT);
     pinMode(3, OUTPUT);
     pinMode(ButtonPin, INPUT);
-    Serial.begin(9600);
 }
 
 void Sweep(){
     int i;
-    Serial.print("Sweep\n");
      for ( i = 0; i < 6; ++i){
         digitalWrite( 13 - 2*i, HIGH);
         delay(freq);
@@ -52,7 +50,6 @@ void SOS(){
 
 void Colors(){
   int i;
-  Serial.print("Colors\n");
   for (i = 0; i < 3; ++i){
       digitalWrite(13 - 2*i, HIGH);
        digitalWrite(13 - 6 - 2*i, HIGH);
@@ -62,21 +59,45 @@ void Colors(){
   }
 }
 
+void OutIn(){
+    int i;
+    for (i =0; i < 3; ++i){
+        switch(i){
+            case 0: 
+                digitalWrite(13, HIGH);
+                digitalWrite(3, HIGH);
+                delay(freq);
+                digitalWrite(13, LOW);
+                digitalWrite(3,LOW);
+            case 1:
+                digitalWrite(11, HIGH);
+                digitalWrite(5, HIGH);
+                delay(freq);
+                digitalWrite(11, LOW);
+                digitalWrite(5,LOW);
+            case 2:
+                digitalWrite(9, HIGH);
+                digitalWrite(7, HIGH);
+                delay(freq);
+                digitalWrite(9, LOW);
+                digitalWrite(7,LOW);
+        }
+    }    
+    delay(freq);
+}
 
 void loop(){
-    Serial.print(i);
     switch (i){
       case 0: Sweep(); break;
-      case 1: SOS(); break;
+      case 1: OutIn(); break;
       case 2: Colors(); break;
-      case 3: break;
+      case 3: SOS();
+      case 4: break;
     }
-    Serial.print("\n");
     if ( digitalRead(ButtonPin) == HIGH){
-        Serial.print("Button Read!\n");
         i++;
         Serial.print(i);
-        if (i > 3) i = 0;
+        if (i > 4) i = 0;
         delay(1000);
     }
     
